50edb8060d4802e1816449d9eade16022d4dec82
ngatttnl <62821812+ngatttnl@users.noreply.github.com>
2020-04-04 20:54:01 +0700

    Updates

---
+++ precache-manifest.289b8eebf3dedb568880feae72430596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a5a03e1c255c7470ce93196a86b8a1",
+    "url": "/ngattt/index.html"
+  },
+  {
+    "revision": "cdbd3291424b19dce5b1",
+    "url": "/ngattt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e626b6e7313ea8377d8",
+    "url": "/ngattt/static/js/2.b80f76bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ngattt/static/js/2.b80f76bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbd3291424b19dce5b1",
+    "url": "/ngattt/static/js/main.e7dccda5.chunk.js"
+  },
+  {
+    "revision": "36daa09ce236d7cc8890",
+    "url": "/ngattt/static/js/runtime-main.47a9ec78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ngattt/static/media/logo.5d5d9eef.svg"
+  }
+]);
